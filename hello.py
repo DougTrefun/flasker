e8f8fc51a349f86be2548317083b6350a9eef66a
Doug T <dtrefun@gmail.com>
2021-06-11 14:02:31 -0400

    updated navbar and forms

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 
 
 # create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "this is a key and it is secret"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+	name = StringField("What's Your Name?", validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
 # Create a route decorator
 @app.route('/')
@@ -38,3 +47,14 @@ def page_not_found(e):
 def page_not_found(e):
 	return render_template("500.html"),500
 
+# Create name page
+@app.route('/name', methods=['GET','POST'])
+def name():
+	name = None
+	form = NamerForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ""
+	return render_template("name.html",
+		name = name,
+		form = form)
